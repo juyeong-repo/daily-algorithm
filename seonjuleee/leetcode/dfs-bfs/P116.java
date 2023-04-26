47017cb5820ee597bbe000328a82960ed7e11a87
seonjuleee <nlsj0820@naver.com>
2023-04-27 06:39:11 +0900

    [docs] 문서 형식 변경

--- seonjuleee/leetcode/dfs-bfs/P116.java
+++ seonjuleee/leetcode/dfs-bfs/P116.java
@@ -1,27 +1,33 @@
-public class P116 {
-    class Solution {
-        public Node connect(Node root) {
-            Queue<Node> queue = new LinkedList<>();
-            queue.add(root);
+/**
+ * 2023.04.26
+ * 116. Populating Next Right Pointers in Each Node
+ * https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+ *
+ * Site: Leetcode
+ * Level: Medium
+ * */
+class Solution {
+    public Node connect(Node root) {
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
 
-            while (!queue.isEmpty()) {
-                int size = queue.size();
-                for (int i=0; i<size; i++) {
-                    Node node = queue.poll();
-                    if (node == null) continue;
-                    if (node.left != null) {
-                        queue.add(node.left);
-                    }
-                    if (node.right != null) {
-                        queue.add(node.right);
-                    }
-                    if (i != size - 1) {
-                        node.next = queue.peek();
-                    }
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i=0; i<size; i++) {
+                Node node = queue.poll();
+                if (node == null) continue;
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+                if (i != size - 1) {
+                    node.next = queue.peek();
                 }
             }
-
-            return root;
         }
+
+        return root;
     }
 }
