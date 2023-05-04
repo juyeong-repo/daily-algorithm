f9bab823de7412755dd4f9f679f65ce45552770d
juyeong-repo <juyeong.park.tech@gmail.com>
2023-05-04 18:55:56 +0900

    [Leetcode] Array Partition

---
+++ juyeong/leetcode/[Leetcode] Array Partition.py
@@ -0,0 +1,17 @@
+# 출처: https://leetcode.com/problems/array-partition/submissions/
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+
+        sum = 0
+        pair = []
+        nums.sort()
+        
+        
+        for n in nums:
+            #앞에서부터 오름차순으로 페어를 만들어 합 계산
+            pair.append(n)
+            if len(pair) == 2:
+                sum += min(pair)
+                pair = []
+                
+        return sums
