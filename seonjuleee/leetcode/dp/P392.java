5dbaf42aa66de4d3956aeb1659e55034dfd636dd
seonjuleee <nlsj0820@naver.com>
2023-06-12 22:18:04 +0900

    [Leetcode] Is Subsequence

---
+++ seonjuleee/leetcode/dp/P392.java
@@ -0,0 +1,23 @@
+/**
+ * 2023.05.20
+ * 392. Is Subsequence
+ * https://leetcode.com/problems/is-subsequence
+ *
+ * Site: Leetcode
+ * Level: Easy
+ * */
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int sIdx = 0; int tIdx = 0;
+
+        while (sIdx < s.length() && tIdx < t.length()) {
+            if (s.charAt(sIdx) == t.charAt(tIdx)) {
+                sIdx++;
+            }
+            tIdx++;
+        }
+
+        return sIdx == s.length();
+    }
+}
