81ed79bc0f29c6a57c48144e7d6c82f8e95c28a2
juyeong-repo <juyeong.park.tech@gmail.com>
2023-10-22 20:35:06 +0900

    [refactor] 풀이 방법 개선, in 을 사용한 탐색 추가

--- juyeong/leetcode/accepted/[Leetcode] Two Sum.py
+++ juyeong/leetcode/accepted/[Leetcode] Two Sum.py
@@ -10,8 +10,9 @@ class Solution1:
                 if nums[i] + nums[j] == target:
                     return [i,j]
 
-
-class Solution3:
+    
+# 첫 번째 수를 뺀 결과 조회 
+class Solution2:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         # dictionary initialize
         nums_map = {}
@@ -25,3 +26,27 @@ class Solution3:
         for i, num in enumerate(nums):
             if target - num in nums_map and i != nums_map[target - num]:
                 return [i, nums_map[target - num]]
+
+
+# solution2를 하나의 for문으로 변경하기 
+class Solution3:
+    nums_map = {}
+    for i , num in enumerate(nums) :
+        if target - num = nums_map:
+            return [nums_map[target - num], i]
+        nums_map[num] = i
+
+
+
+# in을 사용한 탐색 
+class Solution4:
+    # enumerate() : 인덱스, 원소를 동시에 접근 / compliment : 보어
+    for i, n in enumerate(nums) :
+        compliment = target - n
+        
+        if compliment in nums[i+1:]:
+            return [nums.index(n),nums[i+1:].index(compliment) + (i+1)]
+        
+
+
+
