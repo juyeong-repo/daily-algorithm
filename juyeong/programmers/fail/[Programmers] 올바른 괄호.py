c87b1ea63a5f09dfb865015422bf60b887fd4ee6
juyeong-repo <juyeong.park.tech@gmail.com>
2023-10-29 17:32:02 +0900

    [Programmers] 올바른 괄호 (다른 풀이방법 추가)

--- juyeong/programmers/fail/[Programmers] 올바른 괄호.py
+++ juyeong/programmers/fail/[Programmers] 올바른 괄호.py
@@ -1,6 +1,8 @@
 # Link: https://school.programmers.co.kr/learn/courses/30/lessons/12909
 # 스택을 이용해서 풀어야 한다🥹
 
+
+#1. 스택을 이용한 풀이 
 def solution(s):
     Stack = []
     
@@ -14,4 +16,18 @@ def solution(s):
                 Stack.append(bracket)
                 
     return False if Stack else True
+        
+
+
+#2. 파이썬 dictionary 사용한 풀이
+str_dict = {"(":1, ")":-1}
+
+def solution(s):
+    current_val = 0
+    for i in s:
+        current_val += str_dict[i]
+        if current_val < 0:
+            return False
+    return True if current_val == 0 else False 
+
         
