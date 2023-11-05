878bd0d52979cf45991e23d9de8ad349b8f73f3c
seonjuleee <nlsj0820@naver.com>
2023-11-05 22:40:12 +0900

    [Leetcode] Merge Strings Alternately

---
+++ seonjuleee/leetcode/two-pointer/P1768.java
@@ -0,0 +1,27 @@
+/**
+ * 2023.11.05
+ * 1768. Merge Strings Alternately
+ * https://leetcode.com/problems/merge-strings-alternately
+ *
+ * Site: Leetcode
+ * Level: Easy
+ * */
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        int j = 0;
+        while (i < word1.length() && j < word2.length()) {
+            sb.append(word1.charAt(i));
+            sb.append(word2.charAt(j));
+            i++; j++;
+        }
+        if (i < word1.length()) {
+            sb.append(word1.substring(i));
+        }
+        if (j < word2.length()) {
+            sb.append(word2.substring(j));
+        }
+        return sb.toString();
+    }
+}
