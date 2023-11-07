13b3413601ce48658309a3883ca24e834a74509d
juyeong-repo <juyeong.park.tech@gmail.com>
2023-11-07 20:52:50 +0900

    [feat] 피보나치 수_반복문 풀이 추가

--- juyeong/programmers/fail/[Programmers] 피보나치 수.py
+++ juyeong/programmers/fail/[Programmers] 피보나치 수.py
@@ -37,3 +37,14 @@ def fib(n:int, memo: List[int]) -> int:
     memo[0], memo[1] = 0,1
     
     print(fib(n, memo))
+
+
+
+### 다시 보기
+def solution2(n):
+    fibo = [0, 1]
+    for i in range(2, n+1):
+        fibo.append(fibo[i-1] + fibo[i-2])
+            
+    answer = fibo[-1]%1234567
+    return answer
