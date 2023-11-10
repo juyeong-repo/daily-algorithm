042daf7814e213592730490d0a328646526a87a7
juyeong-repo <juyeong.park.tech@gmail.com>
2023-11-10 16:01:45 +0900

    [feat] 스택 사용한 예시 추가

--- juyeong/programmers/fail/[Programmers] 짝지어 제거하기.py
+++ juyeong/programmers/fail/[Programmers] 짝지어 제거하기.py
@@ -2,7 +2,6 @@
 
 """
 같은 알파벳이 짝수개 있으면 된다.
-
 if aaa bbbb cc 이렇게 정렬된다면,
     1) 짝수개인지 확인
     2) 1 리턴
@@ -11,7 +10,7 @@ if aaa bbbb cc 이렇게 정렬된다면,
     정렬했을 때 갯수가 짝수인지만 확인하면 된다. 
 
     for 문 돌면서 count() 함수 적용
-    
+
 """
 
 def solution(n):
@@ -27,7 +26,26 @@ def solution(n):
         print(answer)
 
     return answer
-            
-# 처음 이렇게 작성하면 테스트 케이스 반만 통과한다. 이유는 첫번째 원소만 체크하기 때문
-# 투포인터를 사용해야 할까? -> 경우의 수를 하나씩 따지는거 아니니까.. 사용할 필요X
-# 예전에 스택 사용한 방법 찾아보고 다시 풀기 / 그리고 기존 방법으로도 풀 수 있을 것 같으니까 더 고민하자 
+
+"""
+처음 이렇게 작성하면 테스트 케이스 반만 통과한다. 이유는 첫번째 원소만 체크하기 때문
+    1) 투포인터를 사용해야 할까? -> 경우의 수를 하나씩 따지는거 아니니까 사용할 필요X
+    2) 예전에 스택 사용한 방법 (https://school.programmers.co.kr/learn/courses/30/lessons/12909) 찾아보고 다시풀기
+    3) 위의 코드 개선해서 해결할 수 있을걸 더 고민하자
+"""        
+
+# 딕셔너리로 선언 -> 스택 사용하기 위함
+str_dict = {"(":1, ")":-1}
+
+def solution(s):
+    current_val = 0
+    for i in s:
+        # s = "()()"
+        current_val += str_dict[i] #current_val를 하나씩 증가시킴
+        if current_val < 0:
+            return False # 처음 들어왔을 때 ")" 이면 false 처리
+        return True if curent_val == 0 else False
+            # curent_val == 0 -> 스택에서 나감
+
+
+    
