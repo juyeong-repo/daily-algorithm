f56cc98eef6f41b8c1feb22a2d22d222b92b7e1f
juyeong-repo <juyeong.park.tech@gmail.com>
2023-11-21 07:08:30 +0900

    [Programmers] 구명보트_투포인터 풀이

--- juyeong/programmers/fail/[Programmers] 구명보트.py
+++ juyeong/programmers/fail/[Programmers] 구명보트.py
@@ -12,7 +12,8 @@
     : deque.append(item): item을 데크의 오른쪽 끝에 삽입한다.
     deque.appendleft(item): item을 데크의 왼쪽 끝에 삽입한다.
     deque.pop(): 데크의 오른쪽 끝 엘리먼트를 가져오는 동시에 데크에서 삭제한다.
-    deque.popleft(): 데크의 왼쪽 끝 엘리먼트를 가져오는 동시에 데크에서 삭제한다.    
+    deque.popleft(): 데크의 왼쪽 끝 엘리먼트를 가져오는 동시에 데크에서 삭제한다.   
+
 2.Two pointer
 
 
@@ -24,17 +25,16 @@
 # 가장 무거운 사람, 가장 가벼운 사람을 묶어서 처리
 from collections import deque
 
-from collections import deque 
 
-def solution(people, limit):
+def solution_Deque(people, limit):
     answer = 0
     people = deque (sorted(people, reverse = True))
     
     #베이스 케이스, people 배열은 1 초과
     while len(people) > 1:
-        if people[0] + people[-1] <= limit :
+        if people[0] + people[-1] <= limit :s
             answer += 1
-            people.pop() #오른쪽 끝
+            people.pop() #오른쪽 끝s
             people.popleft() #왼쪽 끝
             
         # limit 초과 안하면, 둘중에 무거운 애 빼자
@@ -42,10 +42,41 @@ def solution(people, limit):
             answer += 1
             people.popleft()
     
-    # 1명만 남았을 경우
+    # 1명만 남았을 경우 -> 베이스라인 생각하지 못함 '^'
     if people :
         answer += 1
         
         
     return answer
         
+
+def solution_two_pointer(people, limit) :
+
+    answer = 0
+
+    # 몸무게 오름차순 정렬
+    people.sort()
+    
+    # start
+    a = 0
+    # end : element 마지막 원소 
+    b = len (people) - 1
+
+    # start, end = 0, len(people)-1
+
+    while a <= b : # 투 포인터가 순회하는 동안/ 인덱스가 같아지는 시점까지 
+
+        answer += 1
+
+        if people [a]  + people [b] <= limit :
+            # start 포인터 이동, 보트의 수 증가 
+            a += 1
+            # end 포인터 감소 
+            b -= 1
+        
+        # 한명만 남았을 때
+        else: 
+            b -=1
+
+
+    return answer
